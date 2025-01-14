383bea7ba4f4bf243bcad128fcb9ac80e23b2270
sciencephile <158610477+sciencephile@users.noreply.github.com>
2025-01-14 16:23:52 -0500

    Delete kingmaker-styling directory

--- kingmaker-styling/scripts/journal-sheet.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * The custom Journal Sheet used for Kingmaker content.
- */
-export class KingmakerJournalSheet extends JournalSheet {
-  constructor(doc, options) {
-    super(doc, options);
-    this.options.classes.push(kingmaker.CSS_CLASS);
-  }
-}
